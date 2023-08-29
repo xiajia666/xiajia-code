ff38717f4aff167073771870691e3955c56d62c3
xiajia <xj1101295740@163.com>
2023-08-29 11:50:53 +0800

    夏嘉666

--- ttt.py
+++ ttt.py
@@ -1,8 +1,2 @@
-world = "python"
-list = []
-for i in world:
-    list.append(i)
-print(list)
-print(list[3])
-finall_4 = [list[i] for i in range((len(list) - 4),(len(list)))]
-print(finall_4)
+import token
+print()
